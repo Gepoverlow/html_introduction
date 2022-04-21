dab555658cd1c05678014d7f8e906c953a28a36f
lucas <gepeoverload@gmail.com>
2022-04-21 12:16:52 +0200

    added functions that display or hide depending on hover status

--- app.js
+++ app.js
@@ -4,17 +4,36 @@ const isHover = (e) => e.parentElement.querySelector(":hover") === e;
 
 function getChildElements(parentId) {
   let children = Array.from(document.querySelector(`${parentId}`).children);
+
   return children;
 }
+
+containers.forEach((container) => {
+  const childs = getChildElements(`.${container.classList[0]}`);
+  let hoverText = document.createElement("h1");
+  hoverText.textContent = "HOVER ME";
+  hoverText.className = "hover";
+
+  childs.forEach((child) => {
+    child.classList.add("hidden");
+    container.appendChild(hoverText);
+  });
+});
 
 containers.forEach((container) =>
   container.addEventListener("mouseover", function checkHovered() {
     const hovered = isHover(container);
     const childs = getChildElements(`.${container.classList[0]}`);
 
-    childs.forEach((child) =>
-      hovered ? child.classList.remove("hidden") : child.classList.add("hidden")
-    );
+    childs.forEach((child) => {
+      if (hovered) {
+        child.classList.remove("hidden");
+        container.querySelector(".hover").classList.add("hidden");
+      } else {
+        child.classList.add("hidden");
+        container.querySelector(".hover").classList.remove("hidden");
+      }
+    });
   })
 );
 
@@ -23,8 +42,14 @@ containers.forEach((container) =>
     const hovered = isHover(container);
     const childs = getChildElements(`.${container.classList[0]}`);
 
-    childs.forEach((child) =>
-      hovered ? child.classList.remove("hidden") : child.classList.add("hidden")
-    );
+    childs.forEach((child) => {
+      if (hovered) {
+        child.classList.remove("hidden");
+        container.querySelector(".hover").classList.add("hidden");
+      } else {
+        child.classList.add("hidden");
+        container.querySelector(".hover").classList.remove("hidden");
+      }
+    });
   })
 );
